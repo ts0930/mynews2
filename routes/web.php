c18f4a23848112ae79a092f9272c2c15ef15e574
櫻井　翼 <ts0930t@yahoo.co.jp>
2020-11-28 05:04:23 +0000

    課題訂正

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix'=>'admin'],function(){
-    Route::get('admin/profile/create','Admin\ProfileController@add');
-    Route::get('admin/profile/sdit','Admin\ProfileController@edit');
-});
-
 Route::group(['prefix' => 'admin'],function(){
-    Route::get('news/create','admin\NewsController@add');
+    Route::get('news/create','Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
 Route::get('XXX','AAAController@bbb');
